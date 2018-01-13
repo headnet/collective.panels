0b501ce9db4ffd4092894d93a0d733a4cdcaa9b8
Sune Broendum Woeller <sune@woeller.dk>
2018-01-13 15:03:01 +0100

    The interfaces: IManagePanels is no longer used to trigger panel management, we work with each panel manager through the plone toolbar menu. Add the toolbar menu interfaces. Standardize browserlayer as generated by the plonecli. Add css_class to Panel. Column spacing and omitting margins are no longer possible settings, removed from IGlobalSettings. TODO: copy over the zcml registration functionality.

--- src/collective/panels/interfaces.py
+++ src/collective/panels/interfaces.py
@@ -1,8 +1,71 @@
 # -*- coding: utf-8 -*-
-"""Module where all interfaces, events and exceptions live."""
-
+from zope import schema
+from zope.browsermenu.interfaces import IBrowserMenu
+from zope.browsermenu.interfaces import IBrowserSubMenuItem
+from zope.interface import Attribute
+from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from zope.container.interfaces import IContained
+from collective.panels import _
+
+#todo: check this:
+from plone.app.portlets.interfaces import IColumn
 
 
 class ICollectivePanelsLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+
+class ILayout(Interface):
+    """Marker for a layout dictionary."""
+
+
+class IPanel(IColumn, IContained):
+    """A portlet panel.
+
+    Register a portlet for this portlet manager type to enable them
+    only for the panel (and not for the regular portlet column
+    manager).
+    """
+
+    layout = Attribute("Assigned layout.")
+
+    heading = Attribute("Panel heading.")
+
+    css_class = Attribute("Panel css class.")
+
+
+class IPanelManagerSubMenuItem(IBrowserSubMenuItem):
+    """The menu item linking to the panel manager menu.
+    """
+
+
+class IPanelManagerMenu(IBrowserMenu):
+    """The panel manager menu.
+
+    TODO: This gets its menu items from the list of possible portlet managers.
+    """
+
+
+class IGlobalSettings(Interface):
+    site_local_managers = schema.Set(
+        title=_(u"Site-local panel managers"),
+        description=_(u"The locations listed here will be assignable "
+                      u"only at sites (typically Plone's site "
+                      u"root, unless local sites are present)."),
+        required=False,
+        value_type=schema.Choice(
+            vocabulary="collective.panels.vocabularies.Managers",
+        )
+    )
+
+    navigation_local = schema.Bool(
+        title=_(u"Use navigation root"),
+        description=_(u"Site-local panel managers will be assignable "
+                      u"on navigation roots instead of only site roots "
+                      u"if you select this option. Check this if you are "
+                      u"using LinguaPlone, collective.multilingual or "
+                      u"similar, and you want per-language Site-local "
+                      u"panel managers."),
+    )
+
